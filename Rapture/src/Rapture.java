4c9a446bed2641cafb8ef2490dbd282c1eead439
bruceacarson <bcarson1@ggc.edu>
2018-03-19 16:44:51 -0400

    Bruce committed these changes

--- Rapture/src/Rapture.java
+++ Rapture/src/Rapture.java
@@ -1,6 +1,12 @@
+
 //Dionne Mitchner Commit
 // Yiet Mai commit
 // Bruce Carson commit
 public class Rapture {
+	
+	public static void main(String []args){
+		System.out.println("Bruce added this printline statement");
+		
+	}
 
 }

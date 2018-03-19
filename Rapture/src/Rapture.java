12aac2d59b65ae71e62bc233656aa90ed7a1c43d
dmitchner <dmitchner@ggc.edu>
2018-03-19 17:04:10 -0400

    Dionne added commit changes

--- Rapture/src/Rapture.java
+++ Rapture/src/Rapture.java
@@ -7,7 +7,7 @@ public class Rapture {
 	public static void main(String []args){
 		System.out.println("Bruce added this printline statement");
 		System.out.println("Yiet added this printline statement");
-		
+		System.out.println("Dionne added this printline statement");
 	}
 
 }

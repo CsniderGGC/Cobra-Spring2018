44d4b5d931f4f93196da8f90dde1130f078570ad
dmitchner <dmitchner@ggc.edu>
2018-04-09 15:09:18 -0400

    Added Character, Monster, and Player classes

---
+++ MedicalRaptureTextAdventureGame/MedicalRaptureTextAdventureGame/medicalRaptureMain/Character.java
@@ -0,0 +1,34 @@
+package medicalRaptureMain;
+
+public class Character {
+	protected int maxHP, health, maxHit;
+	protected String name;
+
+	public Character( int maxHP, int health, int maxHit) {
+	}
+
+	public int getMaxHP() {
+		return maxHP;
+	}
+
+	public void setMaxHP(int maxHP) {
+		this.maxHP = maxHP;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
--- MedicalRaptureTextAdventureGame/MedicalRaptureTextAdventureGame/medicalRaptureMain/Player.java
+++ MedicalRaptureTextAdventureGame/MedicalRaptureTextAdventureGame/medicalRaptureMain/Player.java
@@ -1,15 +1,41 @@
 package medicalRaptureMain;
 
-public class Player {
-    
-    private final String name;
-    
-    public Player(String name){
-        this.name = name;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    
+public class Player extends Character {
+	public final int maxHp = 100;
+	public final int maxHit = 30;
+
+	private final String name;
+
+	public Player(String name, int maxHP, int health, int maxHit) {
+		super(maxHP, health, maxHit);
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static int getMaxhp() {
+		return maxHp;
+	}
+
+	public static int getMaxhit() {
+		return maxHit;
+	}
+
+	public void utilizeItem() {
+		health = health + healthPack;
+	}
+
 }

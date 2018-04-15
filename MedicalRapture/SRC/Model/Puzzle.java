8d0bc9424c19ee19e132f00e4fa595d694005367
bruceacarson <bcarson1@ggc.edu>
2018-04-14 20:27:54 -0400

    Bruce added puzzle

---
+++ MedicalRapture/SRC/Model/Puzzle.java
@@ -0,0 +1,41 @@
+
+public class Puzzle {
+
+	private String name;
+	private String description;
+	private String solution;
+	private String hint;
+	
+public Puzzle(String name, String description, String solution, String hint){
+	this.name = name;
+	this.description = description;
+	this.solution = solution;
+	this.hint = hint;
+}
+
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public String getSolution() {
+	return solution;
+}
+public void setSolution(String solution) {
+	this.solution = solution;
+}
+public String getHint() {
+	return hint;
+}
+public void setHint(String hint) {
+	this.hint = hint;
+}
+}

2a6558757a07d807140f20509591f0644112b2a8
bruceacarson <bcarson1@ggc.edu>
2018-04-18 16:17:16 -0400

    Puzzle Update

--- MedicalRapture/SRC/Model/PuzzleList.java
+++ MedicalRapture/SRC/Model/PuzzleList.java
@@ -14,6 +14,21 @@ public class PuzzleList {
 		
 		Puzzle lookMeUp = new Puzzle("Look Me Up", "Which word in the dictionary is spelled incorrectly?", "Incorrectly","The answer is in the description.");
 	
+		Puzzle funGuy = new Puzzle ("Fun Guy", "What kind of room has no doors?", "A mushroom", "Starts with m and ends with room. MxxxROOM. You can find this outside on the ground and some you can eat.");
+		
+		Puzzle well = new Puzzle ("Well", "What gets wetter and wetter the more it dries?", "A towel","Something you use right after you shower to dry yourself.");
+		
+		Puzzle easyRiddle = new Puzzle("Easy Riddle", "What can travel around the world while staying in a corner?", "A stamp", "You put it on your mail when you need to send it some-where. You can buy it at a post office.");
+		
+		Puzzle notASnake =  new Puzzle("Not a Snake", "What has a head and a tail, but no body?", "A coin", "You can carry it in your pocket. You flip it and it can land on heads or tails.");
+		
+		puzzleList.add(hero);
+		puzzleList.add(yummy);
+		puzzleList.add(lookMeUp);
+		puzzleList.add(funGuy);
+		puzzleList.add(well);
+		puzzleList.add(easyRiddle);
+		puzzleList.add(notASnake);
 		
 	
 	}

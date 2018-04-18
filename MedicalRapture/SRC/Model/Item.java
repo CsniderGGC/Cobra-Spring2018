42e3fa0566ae6be8081fe41f87b351e453aaa794
ymai1713 <ymai@ggc.edu>
2018-04-18 16:29:54 -0400

    yiet added items methods

--- MedicalRapture/SRC/Model/Item.java
+++ MedicalRapture/SRC/Model/Item.java
@@ -4,7 +4,17 @@ public abstract class Item {
 	String itemName;
 	String abilityDiscription;
 	
+	public void Pickup() {};
+	public void Drop() {};
 	public void Use() {};
+	public void Equipe() {};
+	public void Unequiped() {};
+	public void Utilize() {};
+	public void Examine() {};
+	public void Solve() {};
+	public void Quit() {};
+	public void Hint() {};
+	
 
 
 }
